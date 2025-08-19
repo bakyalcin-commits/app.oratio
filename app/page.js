615b53e956ad53f5430ec98509f96189d4cc6f6b
bakyalcin-commits <b.akyalcin@gmail.com>
2025-08-19 20:55:01 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Oratio App Medical Translate</h1>
+      <p>Welcome to Oratio! Start translating your text, images, and PDFs.</p>
+    </main>
+  );
+}
