6ce4fd6f04cb8e79d8b8d5bfa9b6fe3153b341a2
bakyalcin-commits <b.akyalcin@gmail.com>
2025-08-20 01:22:19 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-// next.config.js  (ESM sürüm, CJS YOK)
- /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {},
-  webpack: (config) => {
-    // Burada özel bir şey yapmıyoruz; jimp sadece server bundle'a girer.
-    return config;
-  },
+  webpack: (config) => config,
 };
 
-export default nextConfig; // DİKKAT: export default, module.exports değil
+export default nextConfig;
+
 
