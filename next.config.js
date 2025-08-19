9af8c4e543344466815a9327c5b7173f27023f03
bakyalcin-commits <b.akyalcin@gmail.com>
2025-08-19 23:58:50 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  experimental: {
+    // native/binary modülleri server componentlarda harici tut
+    serverComponentsExternalPackages: ["@napi-rs/canvas"]
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // binary .node dosyalarını webpack'e parse ettirme
+      config.externals = Array.isArray(config.externals)
+        ? [...config.externals, "@napi-rs/canvas"]
+        : config.externals;
+    }
+    return config;
+  }
 };
+
 module.exports = nextConfig;
+
 
