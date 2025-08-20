d6d15836e827438addff5b363a91fd0ee4808b3b
bakyalcin-commits <b.akyalcin@gmail.com>
2025-08-20 04:00:22 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 export const metadata = { title: "Oratio • Medical Translator" };
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
